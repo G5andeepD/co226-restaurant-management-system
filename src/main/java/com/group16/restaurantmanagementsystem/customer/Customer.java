b018c4e7ec7ae2c228f17ccdbc29614065e2c992
G5andeepD <e19063@eng.pdn.ac.lk>
2023-05-29 22:47:39 +0530

    Completed swagger

--- src/main/java/com/group16/restaurantmanagementsystem/customer/Customer.java
+++ src/main/java/com/group16/restaurantmanagementsystem/customer/Customer.java
@@ -16,10 +16,20 @@ public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+
+
     private String firstName;
     private String lastName;
     private String address;
     private String phoneNo;
 
     private String email;
+    public Customer(String firstName, String lastName, String address, String phoneNo, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phoneNo = phoneNo;
+        this.email = email;
+    }
 }
