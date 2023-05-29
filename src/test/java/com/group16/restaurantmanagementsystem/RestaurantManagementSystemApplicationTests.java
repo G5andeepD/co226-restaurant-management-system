ebcab8c7231ea5b3ef80d4b23748d7ee928285bb
G5andeepD <e19063@eng.pdn.ac.lk>
2023-05-29 21:27:10 +0530

    initial commit

---
+++ src/test/java/com/group16/restaurantmanagementsystem/RestaurantManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group16.restaurantmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
